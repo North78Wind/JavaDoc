d5ceb1abe8b13de0e3c02819704daa07adaf2fa1
StafeeV <north78wind@gmail.com>
2022-08-30 15:39:48 -0300

    primer trabajo hecho , gracias

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] orgs) {
+  int ticket = 2000;
+
+   int bonus = ticket / 20;
+        System.out.println("На Ваш счет начислено: " + bonus + " бонусных миль! " );
+    }
+}
